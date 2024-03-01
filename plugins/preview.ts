79b7e7b3efe35af53ad064b25542860b2df9379a
Michael Wallner <m.wallner@neonblack.at>
2024-03-01 15:00:35 +0100

    🩹 fix preview by using nuxt runtime config

--- plugins/preview.ts
+++ plugins/preview.ts
@@ -1,6 +1,7 @@
 export default defineNuxtPlugin((nuxtApp) => {
   const route = useRoute()
-  const preview = route.query?._storyblok || process.env.NUXT_PUBLIC_APP_ENV !== 'production'
+  const runtimeConfig = useRuntimeConfig()
+  const preview = route.query?._storyblok || runtimeConfig.public.appEnv !== 'production'
 
   if (preview) {
     nuxtApp.hook('page:finish', () => {
