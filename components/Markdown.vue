f6bc1f4d5653a45deb2f05746eae4b80b361ac58
Michael Wallner <m.wallner@neonblack.at>
2024-07-29 14:28:01 +0200

    ✨ add markdown component

---
+++ components/Markdown.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import { marked } from 'marked'
+
+const props = defineProps<{ content: string }>()
+
+const html = computed(() => {
+  return marked.parse(props.content)
+})
+
+</script>
+
+<template>
+  <div
+    class="prose"
+    v-html="html"
+  />
+</template>
